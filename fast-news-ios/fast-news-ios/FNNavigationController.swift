99d8c0155fd8fafa658eff810193a1286a9c0dd8
= <nateparro2t@gmail.com>
2016-02-28 14:41:41 -0500

    fixes

---
+++ fast-news-ios/fast-news-ios/FNNavigationController.swift
@@ -0,0 +1,35 @@
+//
+//  FNNavigationController.swift
+//  fast-news-ios
+//
+//  Created by Nate Parrott on 2/28/16.
+//  Copyright © 2016 Nate Parrott. All rights reserved.
+//
+
+import UIKit
+
+class FNNavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
